2acb347953269bcb79f09db0a7592fef4f7f75c5
Raphael Muenster <raphael.muenster@gmail.com>
2019-06-20 16:12:44 +0200

    WIP: Design update

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from './Navbar';
+import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
 
@@ -8,7 +8,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-      <Navbar />
+      <Header />
       <MainContent />
       </div>
     );
